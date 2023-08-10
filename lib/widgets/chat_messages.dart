cb61f3f662a6ee55976f0d55447061cdd3958887
Dilanka Yasith <123799744+DilankaYasith@users.noreply.github.com>
2023-08-10 11:10:39 +0530

    updated the chat screen

---
+++ lib/widgets/chat_messages.dart
@@ -0,0 +1,45 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class ChatMessages extends StatelessWidget {
+  const ChatMessages({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: FirebaseFirestore.instance
+          .collection('chat')
+          .orderBy('createdAt', descending: false)
+          .snapshots(),
+      builder: (ctx, chatSnapshot) {
+        if (chatSnapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+
+        if (!chatSnapshot.hasData || chatSnapshot.data!.docs.isEmpty) {
+          return const Center(
+            child: Text('No messages found'),
+          );
+        }
+
+        if (chatSnapshot.hasError) {
+          return const Center(
+            child: Text('Something went wrong'),
+          );
+        }
+
+        final loadedMessages = chatSnapshot.data!.docs;
+
+        return ListView.builder(
+          padding: const EdgeInsets.fromLTRB(13, 0, 13, 40),
+          reverse: true,
+          itemCount: loadedMessages.length,
+          itemBuilder: (ctx, index) =>
+              Text(loadedMessages[index].data()['text']),
+        );
+      },
+    );
+  }
+}
